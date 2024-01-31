27b3f894b701fb5d1fc3ef7099d067247c493ea1
Rubem Viegas <131497613+Rubemlv@users.noreply.github.com>
2024-01-30 23:18:15 -0300

    Update jslog.js

--- jslog.js
+++ jslog.js
@@ -5,7 +5,7 @@ function validateLogin() {
     // Verificar se o nome de usuário e senha são válidos
     if (username === "eto" && password === "eto@energisa") {
         // Login bem-sucedido, redirecionar ou fazer outras ações necessárias
-        window.location.href = "index.html";
+        window.location.href = "direct.html";
     } else {
         // Exibir mensagem de erro
         document.getElementById("error-message").innerText = "Usuário ou senha incorretos.";
